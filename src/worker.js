b8d784e983b486ac22dc134a22188564e255171d
Parveen Bhadoo <github-notify@hashhackers.com>
2023-07-07 17:23:19 +0530

    Initial Script from ChatGPT to Modify as per needs

--- src/worker.js
+++ src/worker.js
@@ -1,15 +1,39 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+// Cloudflare Worker script to download files from Google Drive API
 
-export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
-};
+addEventListener("fetch", async (event) => {
+  event.respondWith(handleRequest(event.request));
+});
+
+async function handleRequest(request) {
+  const url = new URL(request.url);
+  const path = url.pathname;
+
+  if (path === "/download.aspx") {
+    const fileId = url.searchParams.get("fileId");
+    const accessToken = "YOUR_ACCESS_TOKEN"; // Replace with your Google Drive API access token
+
+    if (fileId) {
+      // Make a request to the Google Drive API to get the file download URL
+      const fileUrl = `https://www.googleapis.com/drive/v3/files/${fileId}?alt=media`;
+      const headers = { Authorization: `Bearer ${accessToken}` };
+      const response = await fetch(fileUrl, { headers });
+
+      // Check if the API request was successful
+      if (response.ok) {
+        // Get the content type from the response headers
+        const contentType = response.headers.get("content-type");
+
+        // Set the content type in the response headers
+        const headers = { "content-type": contentType };
+
+        // Return the file content as the response body
+        return new Response(response.body, { headers });
+      }
+    }
+    // If the file ID is missing or the API request failed, return an error response
+    return new Response("Failed to download file", { status: 500 });
+  }
+
+  // For any other path, return an "OK" status
+  return new Response("OK", { status: 200 });
+}
